d8ce40717aaf0279744428df61fae016fefce197
Matheus Betim <mbetim47@gmail.com>
2024-01-06 14:35:31 -0300

    refactor: move the logic to get the current repo and workspace to an external function

---
+++ pkg/cmd/pr/utils.go
@@ -0,0 +1,25 @@
+package pr
+
+import (
+	"github.com/mbetim/bit/pkg/bitbucket"
+	"github.com/mbetim/bit/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+func GetRepoAndWorkspaceFromCmdOrConfig(cmd *cobra.Command) (string, string) {
+	workspace, _ := cmd.Flags().GetString("workspace")
+	repo, _ := cmd.Flags().GetString("repo")
+
+	if repo == "" {
+		repo, workspace, _ = bitbucket.GetRepoAndWorkspaceNameFromCurrentDir()
+		return repo, workspace
+	}
+
+	if workspace == "" {
+		savedConfig, _ := config.GetConfig()
+
+		workspace = savedConfig.DefaultWorkspace
+	}
+
+	return repo, workspace
+}
