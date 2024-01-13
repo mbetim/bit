4814fc3aaf9efdc46b798da0be92c968c12b10c8
Matheus Betim <mbetim47@gmail.com>
2024-01-13 00:42:29 -0300

    fix: give a better description to the `pr` command

--- pkg/cmd/pr/pr.go
+++ pkg/cmd/pr/pr.go
@@ -1,24 +1,14 @@
 package pr
 
 import (
-	"fmt"
-
 	"github.com/mbetim/bit/pkg/cmd"
 	"github.com/spf13/cobra"
 )
 
 var prCmd = &cobra.Command{
 	Use:   "pr",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("pr called")
-	},
+	Short: "Work with Bitbucket pull requests",
+	Long:  "Work with Bitbucket pull requests",
 }
 
 func init() {
